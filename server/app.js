922278aeec50f043db78169efd4592776aa5324d
Morgan Clements <morganclements@ldschurch.org>
2013-04-29 20:18:17 -0600

    update files

--- server/app.js
+++ server/app.js
@@ -6,9 +6,6 @@ var httpProxy = require('http-proxy');
 
 var app = express();
 
-//here's how to set things that routes may need access to (drivers, loggers, etc)
-app.set('info', {name: "Open Web App"});
-
 //development environment
 app.configure('development', function () {
     //serve the static assets
@@ -17,20 +14,7 @@ app.configure('development', function () {
     app.use(require('requirejs-glob')());
 });
 
-//production environment
-app.configure('production', function () {
-    //serve the optimized static assets
-    app.use(express.static('app-build'));
-});
-
-//middleware
 app.use(express.bodyParser());
-app.use(require('./middleware/logging')());
-
-//routes
-fs.readdirSync(__dirname + '/routes').forEach(function (file) {
-    require('./routes/' + file)(app);
-});
 
 var marklogicProxy = httpProxy.createServer(port, 'localhost');
 
--- test/browser/version-service-test.js
+++
@@ -1,6 +0,0 @@
-describe('version service', function(){
-  beforeEach(module('app'));
-  it('should return current version', inject(function(version) {
-    expect(version).to.equal('0.2.0');
-  }));
-});
